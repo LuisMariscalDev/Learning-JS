53ed96766bc19ce871d0a6ff3286de66505c1d4d
LuisMariscalDev <luisferdev16@gmail.com>
2023-08-11 19:56:05 -0600

    boolean

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,13 @@
+const autenticado = true;
+
+/*
+if(autenticado) {
+    console.log('Sí puede ver Netflix');
+} else {
+    console.log('No puedes ver pelis bb')
+}
+ */
+
+// Operador Ternario
+
+console.log( autenticado ? 'Sí está autenticado' : 'No está autenticado');
