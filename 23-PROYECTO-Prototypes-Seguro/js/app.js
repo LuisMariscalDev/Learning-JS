82cda50c3391c7577440c209f3b0376d8ef5281f
LuisMariscalDev <luisferdev16@gmail.com>
2023-08-21 22:17:57 -0600

    select of year

---
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -0,0 +1,31 @@
+
+// Constructores
+function Seguro(marca, year, tipo) {
+    this.marca = marca;
+    this.year = year;
+    this.tipo = tipo;
+}
+function UI(){}
+
+// Llena las opciones de años
+UI.prototype.llenarOpciones = () => {
+    const max = new Date().getFullYear(),
+          min = max - 20;
+
+    const selectYear = document.querySelector('#year');
+
+    for(let i = max; i >= min; i--) {
+        let option = document.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        selectYear.appendChild(option);
+    }
+}
+
+// Instanciar UI
+const ui = new UI();
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    ui.llenarOpciones(); // Llenar el select con los años
+})
