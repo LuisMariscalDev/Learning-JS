11aa437eec60815ab6c2831a6a9be6ad8f5ff424
LuisMariscalDev <luisferdev16@gmail.com>
2023-08-27 23:02:18 -0600

    first steps

---
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -0,0 +1,37 @@
+// Campos del formulario
+const mascotaInput = document.querySelector('#mascota');
+const propietarioInput = document.querySelector('#propietario');
+const telefonoInput = document.querySelector('#telefono');
+const fechaInput = document.querySelector('#fecha');
+const horaInput = document.querySelector('#hora');
+const sintomasInput = document.querySelector('#sintomas');
+
+// UI
+const formulario = document.querySelector('#nueva-cita');
+const contenedorCitas = document.querySelector('#citas');
+
+// Registrar eventos
+evenListeners();
+function evenListeners() {
+    mascotaInput.addEventListener('input', datosCita);
+    propietarioInput.addEventListener('input', datosCita);
+    telefonoInput.addEventListener('input', datosCita);
+    fechaInput.addEventListener('input', datosCita);
+    horaInput.addEventListener('input', datosCita);
+    sintomasInput.addEventListener('input', datosCita);
+}
+
+// Objeto con los datos de la cita
+const citaObj = {
+    mascota: '',
+    propietario: '',
+    telefono: '',
+    fecha: '',
+    hora: '',
+    sintomas: ''
+}
+
+// Agrega datos del objeto a la cita
+function datosCita(e) {
+    citaObj[e.target.name] = e.target.value;
+}
