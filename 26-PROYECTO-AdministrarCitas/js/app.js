d9de118c2d893438edaaa659f73865014c48c599
LuisMariscalDev <luisferdev16@gmail.com>
2023-08-28 22:39:30 -0600

    button delete

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -17,8 +17,10 @@ class Citas {
 
     agregarCita(cita) {
         this.citas = [...this.citas, cita];
+    }
 
-        console.log(this.citas);
+    eliminarCita(id) {
+        this.citas = this.citas.filter( cita => cita.id !== id)
     }
 }
 
@@ -33,7 +35,7 @@ class UI {
         if(tipo === 'error') {
             divMensaje.classList.add('alert-danger');
         } else {
-            divMensaje.classList.add('alert-succes');
+            divMensaje.classList.add('alert-success');
         }
 
         // Mensaje de error
@@ -89,6 +91,14 @@ class UI {
                 <span class="font-weight-bolder">Síntomas: </span> ${sintomas}
             `;
 
+            // Boton para eliminar estas citas
+            const btnEliminar = document.createElement('button');
+            btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
+            btnEliminar.innerHTML = `Eliminar <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"></path>
+          </svg>`; 
+
+          btnEliminar.onclick = () => eliminarCita(id);
 
             // Agregar los parrafos a la cita
             divCita.appendChild(mascotaParrafo);
@@ -97,6 +107,7 @@ class UI {
             divCita.appendChild(fechaParrafo);
             divCita.appendChild(horaParrafo);
             divCita.appendChild(sintomasParrafo);
+            divCita.appendChild(btnEliminar);
 
             // Agregar citas al HTML
             contenedorCitas.appendChild(divCita);
@@ -180,4 +191,15 @@ function reinciarObjeto() {
     citaObj.fecha = '';
     citaObj.hora = '';
     citaObj.sintomas = '';
+}
+
+function eliminarCita(id) {
+    // Eliminar la cita
+    administrarCitas.eliminarCita(id);
+
+    // Muestre un mensaje
+    ui.imprimirAlerta('La cita se eliminó correctamente');
+
+    // Refrescar las citas
+    ui.imprimirCitas(administrarCitas);
 }
