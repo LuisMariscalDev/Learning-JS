8fbd36dbf3276d0ce57caf73c029c8c5ef945630
LuisMariscalDev <luisferdev16@gmail.com>
2023-08-30 23:03:26 -0600

    added new clients and read clients

--- 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
+++ 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
@@ -32,12 +32,42 @@
 
         if(nombre === '' || email === '' || telefono === '' || empresa === '' ){
             imprimirAlerta('Todos los campos son obligatorios', 'error');
-
             return;
         }
 
+        // Crear un objeto con la informacion
+
+        const cliente = {
+            nombre,
+            email,
+            telefono,
+            empresa,
+        }
+
+        cliente.id = Date.now();
+
+        crearNuevoCliente(cliente);
     }
 
+    function crearNuevoCliente(cliente) {
+        const transaction = DB.transaction(['crm'], 'readwrite');
+
+        const objectStore = transaction.objectStore('crm');
+
+        objectStore.add(cliente);
+
+        transaction.onerror = function() {
+            imprimirAlerta('Algo salió mal', 'error');
+        }
+
+        transaction.oncomplete = function() {
+            imprimirAlerta('Cliente agregado correctamente');
+
+            setTimeout(() => {
+                window.location.href = 'index.html';
+            }, 3000);
+        }
+    }
 
     function imprimirAlerta(mensaje, tipo) {
 
