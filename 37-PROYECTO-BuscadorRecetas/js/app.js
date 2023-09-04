2c0d5077a77196af5f6bf8694d02bc01c55ee9f3
LuisMariscalDev <luisferdev16@gmail.com>
2023-09-03 23:42:24 -0600

    show meals in page

--- 37-PROYECTO-BuscadorRecetas/js/app.js
+++ 37-PROYECTO-BuscadorRecetas/js/app.js
@@ -1,7 +1,9 @@
 function iniciarApp() {
 
-
     const selectCategoria = document.querySelector('#categorias');
+    selectCategoria.addEventListener('change', seleccionarCategoria);
+
+    const resultado = document.querySelector('#resultado');
 
     obtenerCategorias();
 
@@ -14,7 +16,6 @@ function iniciarApp() {
 
     function mostrarCategorias(categorias = []) {
         categorias.forEach( categoria => {
-
             const { strCategory } = categoria;
             const option = document.createElement('option');
             option.value = strCategory;
@@ -22,6 +23,56 @@ function iniciarApp() {
             selectCategoria.appendChild(option);
         })
     } 
+
+    function seleccionarCategoria(e) {
+        const categoria = e.target.value;
+        const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`
+
+        fetch(url)
+            .then( respuesta => respuesta.json())
+            .then( resultado => mostrarRecetas(resultado.meals))
+    }
+
+    function mostrarRecetas(recetas = []) {
+        
+        // Iterar en los resultados
+        recetas.forEach(receta => {
+            const { idMeal, strMeal, strMealThumb } = receta;
+
+            const recetaContenedor = document.createElement('div');
+            recetaContenedor.classList.add('col-md-4');
+
+            const recetaCard = document.createElement('div');
+            recetaCard.classList.add('card', 'mb-4');
+
+            const recetaImagen = document.createElement('img');
+            recetaImagen.classList.add('card-img-top');
+            recetaImagen.alt = `Imagen de la receta de ${strMeal}`;
+            recetaImagen.src = strMealThumb;
+
+            const recetaCardBody = document.createElement('div');
+            recetaCardBody.classList.add('card-body');
+
+            const recetaHeading = document.createElement('h3');
+            recetaHeading.classList.add('card-tittle', 'mb-3');
+            recetaHeading.textContent = strMeal;
+
+            const recetaButton = document.createElement('button');
+            recetaButton.classList.add('btn', 'btn-danger', 'w-100');
+            recetaButton.textContent = 'Ver Receta';
+
+            // Inyectar en el código HTML
+            recetaCardBody.appendChild(recetaHeading);
+            recetaCardBody.appendChild(recetaButton);
+
+            recetaCard.appendChild(recetaImagen);
+            recetaCard.appendChild(recetaCardBody);
+
+            recetaContenedor.appendChild(recetaCard);
+
+            resultado.appendChild(recetaContenedor);
+        })
+    }
 }
 
 document.addEventListener('DOMContentLoaded', iniciarApp);
