13f91c15306c5c94465ec3fd627d23be2c93d699
LuisMariscalDev <luisferdev16@gmail.com>
2023-10-17 22:08:11 -0600

    delete button

--- 44-PROYECTO-CRM-CRUD-REST/js/API.js
+++ 44-PROYECTO-CRM-CRUD-REST/js/API.js
@@ -1,5 +1,6 @@
 const url = 'http://localhost:4000/clientes';
 
+// Cuando se crea un nuevo cliente
 export const nuevoCliente = async cliente => {
     
     try {
@@ -13,5 +14,27 @@ export const nuevoCliente = async cliente => {
         window.location.href = 'index.html';
     } catch (error) {
         console.log(error);
+    }
+}
+
+// Obtiene todos los clientes
+export const obtenerClientes = async () => {
+    try {
+        const resultado = await fetch(url);
+        const clientes = await resultado.json();
+        return clientes;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Elimina un Cliente...
+export const eliminarCliente = async id => {
+    try {
+        await fetch(`${url}/${id}`, {
+            method: 'DELETE'
+        }); 
+    } catch (error) {
+        console.log(error)
     }
 }
