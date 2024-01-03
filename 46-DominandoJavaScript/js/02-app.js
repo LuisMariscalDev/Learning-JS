68305eb30cb3b2d36f50b1e6ca10a6b69dfc2ab3
Luis Mariscal <luisferdev16@gmail.com>
2024-01-02 21:55:14 -0600

    Hoisting

---
+++ 46-DominandoJavaScript/js/02-app.js
@@ -0,0 +1,10 @@
+obtenerCliente('Luis');
+
+function obtenerCliente(nombre) {
+    console.log(`El nombre del cliente es: ${nombre}`);
+}
+
+
+const obtenerCliente2 = function(nombre) {
+    console.log(`El nombre del cliente es: ${nombre}`);
+}
